9b810f8a4f60d1ae1600c2af31dc76e37a31ba43
BecerraIgnacio <ignaciobcerra@gmail.com>
2025-05-05 11:13:56 -0300

    Ejercicio 2 - correcciones

--- ejercicio2.py
+++ ejercicio2.py
@@ -5,41 +5,43 @@ Usuario adivina, devuelve si mayor o menor, hasta que usuario acierta
 import random
 
 def main():
+    while True:
+        jugar_adivinar()
+        if input("¿Otra partida? (s/n): ").lower() != 's':
+            break
+
+def jugar_adivinar():
     print("- - - ADIVINE EL NUMERO - - -\n\n"
-          "Genere un numero aleatorio entre 1 y 100 (inclusive)\n"
+          "Genere un numero aleatorio entre 1 intento 100 (inclusive)\n"
           "Debes adivinarlo!\n"
           "Dime un numero, si no adivinas te dire si mi numero es mayor o menor\n\n")
-    n = random.randint(1,100)
-    y = 0
+    secreto = random.randint(1,100)
+    intento = 0
     while True:
-        x = ingresar_numero()
-        if n == x:
+        numero = ingresar_numero()
+        intento = intento+1
+        if secreto == numero:
             print("Acertaste!")
-            print(f"El numero era {n}, te tomo {y} oportunidades acertar!")
+            print(f"El numero era {secreto}, te tomo {intento} oportunidades acertar!")
             break
-        elif x > n:
-            print(f"No acertaste! Mi numero es menor a {x}")
-        elif x < n:
-            print(f"No acertaste! Mi numero es mayor a {x}")
-        y = y+1
+        elif numero > secreto:
+            print(f"No acertaste! Mi numero es menor a {numero}")
+        elif numero < secreto:
+            print(f"No acertaste! Mi numero es mayor a {numero}")
 
-    print("\n\nVolver a jugar? (1) Si, (2) No")
-    if int(input("Ingrese numero: ")) == 1:
-        main()
-    else:
-        exit()
 
 def ingresar_numero():
     entrada = input("\nIngrese un numero: ")
     try:
         x = int(entrada)
-        if x > 0 or x < 101:
+        if 1 <= x <= 100:
             return x
         else:
             print("El numero tiene que ser un numero entre 1 y 100")
             return ingresar_numero()
     except ValueError:
         print("El numero ingresado no valido")
+        return ingresar_numero()
 
 
 
